257ecc770f9660c85fe639356b4b91cfe74352dc
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-15 14:19:58 +0000

    test: passing test for updateQuality not allowing item quality to pass below 0

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,16 +1,30 @@
 
 describe("Gilded Rose", function() {
 
-  it("should update item quality", function() {
+  it("reduces item quality over time", function() {
     const gildedRose = new Shop([ new Item("Bread", 2, 2) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(1);
   });
 
-  it("should update item sellIn", function() {
+  it("updates item sellIn", function() {
     const gildedRose = new Shop([ new Item("Bread", 2, 2) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toEqual(1);
+  });
+
+  it("degrades quality x2 speed once SellIn passes", function() {
+    const gildedRose = new Shop([ new Item("rottenBread", 0, 2) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  }); 
+
+    it("does not allow item quality to go < 0", function() {
+    const gildedRose = new Shop([ new Item("rottenBread", 0, 2) ]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
   });  
 
 
