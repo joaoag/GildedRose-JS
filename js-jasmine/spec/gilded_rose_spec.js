b1b7aa698723b9f0faf662b256348d718ac7c68a
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-16 16:40:00 +0000

    tests: refactored tests for Shop#updateQuality to use mocks of the Item class

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,40 +1,45 @@
 
 describe("Gilded Rose", function() {
 
-    it("reduces item quality over time", function() {
+    describe("normal items passed through #updateQuality have their:", function(){
+    it("quality initially reduced by 1", function() {
+        const mockItemBread = { name: "Bread", sellIn: 15, quality: 5 }
         const gildedRose = new Shop([ 
-            new Item("Bread", 2, 2) 
+            mockItemBread
         ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).toEqual(1);
+        expect(items[0].quality).toEqual(4);
     });
 
-    it("reduces item sellIn over time", function() {
+    it("sellIn reduced by 1", function() {
+        const mockItemBread = { name: "Bread", sellIn: 15, quality: 5 }
         const gildedRose = new Shop([ 
-            new Item("Bread", 2, 2) 
+            mockItemBread 
         ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].sellIn).toEqual(1);
+        expect(items[0].sellIn).toEqual(14);
     });
 
-    it("degrades quality x2 speed once SellIn passes", function() {
+    it("quality reduced by 2 if SellIn <= 0", function() {
+        const mockItemRottenBread = { name: "Rotten Bread", sellIn: 0, quality: 2 }
         const gildedRose = new Shop([ 
-            new Item("rottenBread", 0, 2) 
+            mockItemRottenBread
         ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(0);
     }); 
 
-    it("does not allow item quality to go < 0", function() {
+    it("quality stopped from being < 0", function() {
+        const mockItemRottenBread = { name: "Rotten Bread", sellIn: 0, quality: 2 }
         const gildedRose = new Shop([ 
-            new Item("rottenBread", 0, 2) 
+            mockItemRottenBread
         ]);
         gildedRose.updateQuality();
         gildedRose.updateQuality();
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(0);
     });
-
+})
     it("updateQuality *increases* the quality of Brie over time", function() {
         const gildedRose = new Shop([
             new Item("Aged Brie", 0, 1) 
@@ -54,7 +59,9 @@ describe("Gilded Rose", function() {
     });
 
     it("updateQuality knows that Sulfuras's quality is 80", function() {
-        const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ]);
+        const gildedRose = new Shop([
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80) 
+        ]);
         gildedRose.updateQuality();
         gildedRose.updateQuality();
         const items = gildedRose.updateQuality();
@@ -62,7 +69,9 @@ describe("Gilded Rose", function() {
     });
   
     it("updateQuality never decreases 'Sulfuras, Hand of Ragnaros's quality", function() {
-        const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 50) ]);
+        const gildedRose = new Shop([
+        new Item("Sulfuras, Hand of Ragnaros", 0, 50) 
+        ]);
         gildedRose.updateQuality();
         gildedRose.updateQuality();
         const items = gildedRose.updateQuality();
@@ -70,7 +79,9 @@ describe("Gilded Rose", function() {
     });
 
     it("updateQuality never decreases 'Sulfuras, Hand of Ragnaros's sellIn", function() {
-        const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 50, 50) ]);
+        const gildedRose = new Shop([
+        new Item("Sulfuras, Hand of Ragnaros", 50, 50) 
+        ]);
         gildedRose.updateQuality();
         gildedRose.updateQuality();
         const items = gildedRose.updateQuality();
@@ -78,47 +89,59 @@ describe("Gilded Rose", function() {
     });
 
     it("updateQuality increases Backstage pass quality as sellIn approaches", function() {
-        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10) ]);
+        const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10) 
+        ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(11);
     });
 
     it("updateQuality increases Backstage pass quality by 2 when sellIn is < 10", function() {
-        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10) ]);
+        const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10) 
+        ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(12);
     });
 
     it("updateQuality increases Backstage pass quality by 3 when sellIn is < 5", function() {
-        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10) ]);
+        const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10) 
+        ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(13);
     }); 
 
     it("updateQuality reduces Backstage pass quality to 0 when sellIn passes", function() {
-        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) ]);
+        const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) 
+        ]);
         gildedRose.updateQuality();
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(0);
     }); 
 
     it("updateQuality reduces conjured items' quality twice as fast as normal items", function() {
-        const gildedRose = new Shop([ new Item("Conjured item", 10, 10) ]);
+        const gildedRose = new Shop([
+        new Item("Conjured item", 10, 10) 
+        ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(8);
     });
 
     it("updateQuality reduces conjured items' quality by 4 once SellIn passes", function() {
-        const gildedRose = new Shop([ new Item("Conjured item", 0, 10) ]);
+        const gildedRose = new Shop([
+        new Item("Conjured item", 0, 10) 
+        ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(6);
     });
 
     it("updateQuality keeps conjured items' quality >= 0", function() {
-        const gildedRose = new Shop([ new Item("Conjured item", 0, 1) ]);
-        console.log(gildedRose.items[0]);
+        const gildedRose = new Shop([
+        new Item("Conjured item", 0, 1) 
+        ]);
         const items = gildedRose.updateQuality();
-        console.log(gildedRose.items[0]);
         expect(items[0].quality).toEqual(0);
     });
 
