2548625f9f0f48e9ac9bb8d8296a08d6bf190c73
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-16 14:47:17 +0000

    test: unit test for shop initialise properties passing

---
+++ js-jasmine/spec/shop_spec.js
@@ -0,0 +1,8 @@
+describe("Gilded Rose: Shop", function() {
+
+  it("should initialise with an empty array called 'items'", function() {
+    const gildedRose = new Shop();
+    expect(gildedRose.items).toEqual([]);
+  });
+
+});
