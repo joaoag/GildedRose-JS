f2fcfa9e808c39c3e5076658d0640035d41a7045
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-17 10:48:55 +0000

    refactor: conjuredUpdate class w #calculateQuality added, tests passing

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -7,12 +7,13 @@ class Item {
 }
 
 class Shop {
-    constructor(items=[], normal = new NormalUpdate, brie = new BrieUpdate, sulfuras = new SulfurasUpdate, pass = new PassesUpdate){
+    constructor(items=[], normal = new NormalUpdate, brie = new BrieUpdate, sulfuras = new SulfurasUpdate, pass = new PassesUpdate, conjured = new ConjuredUpdate){
         this.items = items;
         this.normal = normal;
         this.brie = brie;
         this.sulfuras = sulfuras;
         this.pass = pass;
+        this.conjured = conjured;
     }
 
     updateQuality() {
@@ -31,7 +32,7 @@ class Shop {
         } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
             this.pass.calculateQuality(item);
         } else if (item.name == "Conjured item") {
-            this.calculateConjuredQuality(item);
+            this.conjured.calculateQuality(item);
         } else {
             this.normal.calculateQuality(item);
         }
@@ -41,17 +42,5 @@ class Shop {
         item.name != "Sulfuras, Hand of Ragnaros" ? item.sellIn -= 1 : this.sulfuras.SellIn(item);
     }
 
-    calculateConjuredQuality(item){
-        if (item.quality >= 50) {
-            item.quality = 50;
-        } else if (item.sellIn <= 0 && item.quality <= 0) {
-            item.quality = 0;
-        } else if (item.sellIn <= 0 && item.quality <= 4) {
-            item.quality = 0 ;
-        } else if (item.sellIn <= 0 && item.quality > 4) {
-            item.quality -= 4 ;
-        } else {
-            item.quality -= 2;
-        }
-    }
+
 }
