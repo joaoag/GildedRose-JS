19dd5c05d32f88243eb6c67e27e3696c496307be
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-17 11:09:30 +0000

    refactor: sell-in class added with #reduce, all tests passing

--- js-jasmine/src/Shop.js
+++ js-jasmine/src/Shop.js
@@ -7,21 +7,18 @@ class Item {
 }
 
 class Shop {
-    constructor(items=[],sort = new Sort){
+    constructor(items=[],sort = new Sort, sellin = new SellIn){
         this.items = items;
         this.sort = sort;
+        this.sellin = sellin
     }
 
     updateQuality() {
         for (var i = 0; i < this.items.length; i ++) {
-            this.reduceSellIn(this.items[i]);
+            this.sellin.reduce(this.items[i]);
             this.sort.reduceQuality(this.items[i]);
         }
         return this.items;
-    }
-
-    reduceSellIn(item) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn -= 1;
     }
 
 }
